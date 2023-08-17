a27f1fc4c51dd32298275952aa0d92df0610519d
Ross Brewster <ross.brewster14@gmail.com>
2023-08-17 00:18:20 +0000

    Ross Brewster - Completed prep-string-challenge

---
+++ prep-string-challenge/src/is-vowel.js
@@ -0,0 +1,8 @@
+/* exported isVowel */
+function isVowel(character) {
+  let char = character;
+  char = char.toUpperCase();
+  return (
+    char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
+  );
+}
